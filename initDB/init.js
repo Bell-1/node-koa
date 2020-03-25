e466cc4aba840d82ec84f244cb5b7b2db91420ea
Bell <Bell@yeeuu.com>
2020-03-25 11:11:36 +0800

    调整

--- initDB/init.js
+++
@@ -1,9 +0,0 @@
-const mongoDB = require('../mongodb/db');
-const db = mongoDB.db;
-const status = mongoDB.status;
-const addOpenedCallback = mongoDB.addOpenedCallback;
-
-
-function createCollection(name, cols){
-	db.createCollection(name, cols)
-}
